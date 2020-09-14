b8027892264fa9a367c531927f8a26bee4a82b97
James <35672279+Jatkisson@users.noreply.github.com>
2020-09-13 21:21:55 -0500

    Add files via upload

---
+++ Lab02Program1B/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+/**
+ * @author James Atkisson
+ * Class Program that displays states and their capital cities*/
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Capital\t\t\tState\n" +
+            "Topeka\t\t\tKansas\n" +
+            "Oklahoma City\t\tOklahoma\n" +
+            "Jefferson City\t\tMissouri\n" +
+            "Des Moines\t\tIowa\n");
+    }
+}
---
+++ Lab02Program2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+/**
+ * @author James Atkisson
+ * Class Program that displays the author's initials via the console*/
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Prompt the user for the first integer
+        Console.Write("Please enter a positive integer: ");
+        int firstInt = Convert.ToInt16(Console.ReadLine());
+
+        //Prompt the user for the second integer
+        Console.Write("Please enter another positive integer: ");
+        int secondInt = Convert.ToInt16(Console.ReadLine());
+
+        //Output of the arithmetic operations
+        //Output for addition operator
+        Console.WriteLine($"Sum: {firstInt + secondInt:N0}");
+
+        //Output for multiplication operator
+        Console.WriteLine($"Product: {firstInt * secondInt:N0}");
+
+        //Output for subtraction operator
+        Console.WriteLine($"Difference: {firstInt - secondInt:N0}");
+
+        //Output for division operator 
+        //Integers are initially converted to doubles for more precision 
+        Console.WriteLine($"Quotient : " +
+            $"{Convert.ToDouble(firstInt) / Convert.ToDouble(secondInt):F2}");
+
+        //Output for the remainder operator
+        Console.WriteLine($"Remainder: {firstInt % secondInt:N0}");
+    }
+}
