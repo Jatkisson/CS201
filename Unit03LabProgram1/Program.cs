45c76961b4707262f9a081fc95aef4f594655cfb
James <35672279+Jatkisson@users.noreply.github.com>
2020-09-13 21:24:10 -0500

    Add files via upload

---
+++ Unit03LabProgram1/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+/**
+ * @author James Atkisson
+ * Class Program to compute an area or circumference given 
+ * a user-entered radius and desired choice. */
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Call the function to get the radius
+        double radius = PromptUserValue("radius (feet)");
+
+        // Ensure the radius is valid
+        if (radius > 0)
+        {
+            //Get the user choice of Area or Circumference
+            String areaOrCirc = PromptAreaOrCirc();
+
+            if (areaOrCirc == "a" || areaOrCirc == "A")
+            {
+                double area = Math.PI * radius * radius;
+                Console.Write($"Area: {area:N4} square feet");
+            }
+            else if (areaOrCirc == "c" || areaOrCirc == "C")
+            {
+                double circumference = 2 * Math.PI * radius;
+                Console.Write($"Circumference: {circumference:N4} feet");
+            }
+            else
+            {
+                Console.Write("I'm sorry, I did not " +
+                    "understand your choice.");
+            }
+        }
+        else
+        {
+            Console.Write("I'm sorry, an invalid radius was entered.");
+        }
+
+    }
+
+    static public double PromptUserValue(string valuePrompted)
+    {
+        //Prompt the user for the first integer
+        Console.Write($"Enter the {valuePrompted}: ");
+        double value = Convert.ToDouble(Console.ReadLine());
+
+        return value;
+    }
+
+    static public String PromptAreaOrCirc()
+    {
+
+        //Prompt the user for A (Area) or C (Circumference)
+        Console.Write("Enter A (Area) or C (Circumference): ");
+        String selection = (Console.ReadLine());
+        
+        return selection;
+    }
+
+}
---
+++ Unit03LabProgram2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+/**
+ * @author James Atkisson
+ * Class Program that will display the season which 
+ * corresponds to a user-entered month.*/
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Prompt the user for the month
+        Console.WriteLine("Enter a numeric month and " +
+            "I will display the season.");
+        Console.Write("Month: ");
+        int month = Convert.ToInt16(Console.ReadLine());
+
+        //Write the corresponding season or an error message
+        switch (month)
+        {
+            case 12:
+            case 1:
+            case 2:
+                Console.Write("\nwinter");
+                break;
+            case 3:
+            case 4:
+            case 5:
+                Console.Write("\nspring");
+                break;
+            case 6:
+            case 7:
+            case 8:
+                Console.Write("\nsummer");
+                break;
+            case 9:
+            case 10:
+            case 11:
+                Console.Write("\nfall");
+                break;
+            default:
+                Console.Write("\nI’m sorry, an invalid month was entered.");
+                break;
+        }
+
+    }
+}
