f6e9776a709b187c46f263732e54b8bed597efab
James <35672279+Jatkisson@users.noreply.github.com>
2020-09-13 21:19:55 -0500

    Add files via upload

---
+++ FirstCSharpConsoleApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+/**
+ * @author James Atkisson
+ * Class Program that displays Welcome text on run*/
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Welcome to C#!");
+
+        "JCCC".Equals("jccc");
+
+        int i = 1;
+
+        Console.Write("Enter integer one: ");
+        if (Int32.TryParse(Console.ReadLine(), out i))
+            Console.WriteLine($"Converted to int: {i}");
+        else
+            Console.WriteLine($"Improper Integer. Value of i: {i}");
+    }
+}   
---
+++ Lab02Program1A/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+/**
+ * @author James Atkisson
+ * Class Program that displays the author's initials via the console*/
+
+class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("  JJJJJJJJJ\tRRRRRRRRR\tAAAAAAAAA");
+            Console.WriteLine("      J    \tR       R\tA       A");
+            Console.WriteLine("      J    \tRRRRRRRRR\tAAAAAAAAA");
+            Console.WriteLine("      J    \tR     R  \tA       A");
+            Console.WriteLine("  J   J    \tR      R \tA       A");
+            Console.WriteLine("  JJJJJ    \tR       R\tA       A");
+    }
+    }
