d8e0d1798c3c131024fdede234647bc5136a4620
James <atkisson.james@gmail.com>
2020-09-14 21:43:44 -0500

    Added unit tests to most recent lab project

--- LoanTracker/Program.cs
+++ LoanTracker/Program.cs
@@ -1,12 +1,29 @@
 ﻿using System;
+using System.IO;
 
-namespace LoanTracker
+/**
+ * @author James Atkisson
+ * Class Program assists end user with managing loans*/
+
+class Program
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        try
         {
-            Console.WriteLine("Hello World!");
+            // Open the text file using a stream reader.
+            using (var sr = new StreamReader("LoanFileJamesA.txt"))
+            {
+                // Read the stream as a string, and write the string to the console.
+                Console.WriteLine(sr.ReadToEnd());
+            }
         }
+        catch (IOException e)
+        {
+            Console.WriteLine("The file could not be read:");
+            Console.WriteLine(e.Message);
+        }
+
     }
+ 
 }
